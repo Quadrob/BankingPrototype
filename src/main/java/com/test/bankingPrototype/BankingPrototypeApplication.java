c2ed5760cc4aebbf4a0199733eb6a18a5e4765cb
Robert-Zeelie <robert.zeelie@s4.co.za>
2023-07-01 13:46:12 +0200

    Initial commit to setup the spring boot api foundation

---
+++ src/main/java/com/test/bankingPrototype/BankingPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.test.bankingPrototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/bankingPrototype/BankingPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.bankingPrototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
